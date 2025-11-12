768cb482d63913216058093f452a02bf1cf6a02e
Daniel Chassagnez Goncalves <daniel.chassagn@gmail.com>
2025-11-12 14:53:49 +0100

    fix token

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 require('dotenv').config();
 
-const APIToken = process.env.APIToken;
+const APIToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMjNmYzlmYzZiMDZjYTViZGYyZTdjY2Q5Y2U0ZGIzZSIsIm5iZiI6MTc2MjkzODg5MC44NzM5OTk4LCJzdWIiOiI2OTE0NTAwYTNjZDgwY2UxN2YzYTNiN2MiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.spu7Gqhah5FQ0Xy6wpDhebNJ_GjWPH6Twl_eJXXImKM';
 
 router.get('/movies', async (req, res) => {
   try {
