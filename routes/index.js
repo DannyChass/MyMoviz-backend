01fcab614c45f5f385f11d4afd0618deb4ca9a01
Daniel Chassagnez Goncalves <daniel.chassagn@gmail.com>
2025-11-12 14:57:42 +0100

    fix: don't use dotenv

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-require('dotenv').config();
 
 const APIToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMjNmYzlmYzZiMDZjYTViZGYyZTdjY2Q5Y2U0ZGIzZSIsIm5iZiI6MTc2MjkzODg5MC44NzM5OTk4LCJzdWIiOiI2OTE0NTAwYTNjZDgwY2UxN2YzYTNiN2MiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.spu7Gqhah5FQ0Xy6wpDhebNJ_GjWPH6Twl_eJXXImKM';
 
